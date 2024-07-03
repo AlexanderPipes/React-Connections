893b4c4d42989bd5500fd0b04263d87c36671334
Alex <alexkpipes@gmail.com>
2024-07-03 12:01:46 -0400

    Shuffle Button and Css Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,187 @@
 import { act, useState } from 'react';
 
-const names = [
-  "dan","is","very","drunk","Liam","is","extremely","cringe","Osully","is","not","6'","Tom","is","super","dumb"
-]
-
-const matches = [
-  [0,1,2,3],
-  [4,5,6,7],
-  [8,9,10,11],
-  [12,13,14,15]
+const initialButtonState = [
+  {
+    buttonName: "Blue",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Colors",
+    buttonID: 1
+  },
+  {
+    buttonName: "Red",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Colors",
+    buttonID: 2
+  },
+  {
+    buttonName: "Green",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Colors",
+    buttonID: 3
+  },
+  {
+    buttonName: "Yellow",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Colors",
+    buttonID: 4
+  },
+  {
+    buttonName: "1",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Numbers",
+    buttonID: 5
+  },
+  {
+    buttonName: "2",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Numbers",
+    buttonID: 6
+  },
+  {
+    buttonName: "3",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Numbers",
+    buttonID: 7
+  },
+  {
+    buttonName: "4",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Numbers",
+    buttonID: 8
+  },
+  {
+    buttonName: "A",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Letters",
+    buttonID: 9
+  },
+  {
+    buttonName: "B",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Letters",
+    buttonID: 10
+  },
+  {
+    buttonName: "C",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Letters",
+    buttonID: 11
+  },
+  {
+    buttonName: "D",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "Letters",
+    buttonID: 12
+  },
+  {
+    buttonName: "Wombo",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "That ain't falco",
+    buttonID: 13
+  },
+  {
+    buttonName: "Feet",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "That ain't falco",
+    buttonID: 14
+  },
+  {
+    buttonName: "Wombo",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "That ain't falco",
+    buttonID: 15
+  },
+  {
+    buttonName: "Combo",
+    isActive: false,
+    isEnabled: true,
+    matchReason: "That ain't falco",
+    buttonID: 16
+  },
 ]
 
 // when button is enabled is false I want the class to = inactive square disable
-function Square( {value, onSquareClick, buttonState} ) {
+function Square({ onSquareClick, buttonState }) {
   let classStr = buttonState.isActive ? "active square" : "inactive square";
-  let disable = buttonState.isEnabled ? "" : "disabled";
-  let match = buttonState.isEnabled ? "" : "osully isn't 6'";
+  let disable = buttonState.isEnabled ? "" : "disabled";;
   return <button className={classStr} onClick={onSquareClick} disabled={disable}>
-    {value}
-    </button>;
+    {buttonState.buttonName}
+  </button>;
 }
 
-function SubmitButton( {onSubClick, activeButtons} ) {
+function SubmitButton({ onSubClick, activeButtons }) {
   let disable = activeButtons.length === 4 ? "" : "disabled";
-  return <button className="bttn" onClick={onSubClick} disabled = {disable}>
+  return <button className="bttn" onClick={onSubClick} disabled={disable}>
     Submit
-    </button>;
+  </button>;
 }
 
-function DeselectButton( {onResClick, activeButtons} ) {
+function DeselectButton({ onResClick, activeButtons }) {
   let disable = activeButtons.length !== 0 ? "" : "disabled";
-  return <button className="bttn" onClick={onResClick} disabled = {disable}>
+  return <button className="bttn" onClick={onResClick} disabled={disable}>
     Deselect
-    </button>;
+  </button>;
 }
-function Board(  ) {
-  const [gameState, setGameState] = useState(Array(16).fill( {
-    isActive: false,
-    isEnabled: true,
-    matchReason: ""
-  }));
-   
-  
+
+function ShuffleButton({ onShufClick, activeButtons }) {
+ 
+  return <button className="bttn" onClick={onShufClick} >
+    Shuffle
+  </button>;
+}
+
+function Board() {
+  const [gameState, setGameState] = useState(initialButtonState);
+
+
   function handleClick(i) {
-    const count = gameState.filter(buttonState => {return buttonState.isActive;}).length ;
-    if(count>=4 && gameState[i].isActive === false) { 
+    const count = gameState.filter(buttonState => { return buttonState.isActive; }).length;
+    if (count >= 4 && gameState[i].isActive === false) {
       return;
     } else {
-      //let nextActive = JSON.parse(JSON.stringify(gameState));dsfsdf
+      //let nextActive = JSON.parse(JSON.stringify(gameState));
+      let nextActive = gameState.map(a => structuredClone(a));
       nextActive[i].isActive = !gameState[i].isActive;
       setGameState(nextActive);
       console.log(nextActive);
-      const newCount = nextActive.filter(buttonState => {return buttonState.isActive;}).length;
-      console.log(newCount)
+      console.log(gameState);
     }
   }
 
 
   function checkMatch() {
-    const trueIndex = getTrueIndex()
-    for(let i=0; i<matches.length; i++) {
-      const [a, b, c, d] = matches[i];
-      if (trueIndex[0] === a && trueIndex[1] === b && trueIndex[2] === c && trueIndex[3] === d) {
-        let nextEnabled = gameState.map(old => structuredClone(old))
-        let winningCombo = matches[i]
-        for(let j=0; j<winningCombo.length; j++) {
-          let position = winningCombo[j];
-          nextEnabled[position].isActive = false;
-          nextEnabled[position].isEnabled = false;
-        }
-          setGameState(nextEnabled); 
-        break;
-      } else if(i === matches.length-1) {
-        alert("Not a match!");
-        deselect();
+    let nextEnabled = gameState.map(old => structuredClone(old));
+    let activeArray = gameState.filter(item => item.isActive);
+    for (let i = 0; i < nextEnabled.length; i++) {
+      if (activeArray[0].matchReason !== activeArray[1].matchReason || activeArray[1].matchReason !== activeArray[2].matchReason ||
+        activeArray[2].matchReason !== activeArray[3].matchReason) {
+        console.log("not a match");
+        deselect();ewf
+        return;
       }
     }
+    let trueIndex = getTrueIndex();
+    for (let j = 0; j < trueIndex.length; j++) {
+      let position = trueIndex[j];
+      nextEnabled[position].isActive = false;
+      nextEnabled[position].isEnabled = false;
+    }
+    setGameState(nextEnabled);
   }
 
   function deselect() {
@@ -86,56 +192,67 @@ function Board(  ) {
 
   function getTrueIndex() {
     let trueArray = []
-    for( let i=0; i<gameState.length; i++) {  
+    for (let i = 0; i < gameState.length; i++) {
       if (gameState[i].isActive === true) {
         trueArray.push(i);
       }
     }
     return trueArray;
   }
-  
+
+  function shuffle() {
+    //get the gameState objects
+    //copy the entire gameState and set to shuffledArray variable
+    //shuffle the entire array so that each button has a new index
+    let shuffledArray = gameState.map(old => structuredClone(old));
+    console.log(shuffledArray);
+    let currentIndex = shuffledArray.length;
+    console.log(currentIndex);
+    while(currentIndex !== 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [shuffledArray[currentIndex],shuffledArray[randomIndex]] = [shuffledArray[randomIndex],shuffledArray[currentIndex]]
+      
+    }
+    console.log(shuffledArray);
+    setGameState(shuffledArray);
+  }
   return (
-    <>
-      <div className="board-game">
-        <div className = "board-row">
-          <Square value={names[0]} buttonState={gameState[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={names[1]} buttonState={gameState[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={names[2]} buttonState={gameState[2]} onSquareClick={() => handleClick(2)} />
-          <Square value={names[3]} buttonState={gameState[3]} onSquareClick={() => handleClick(3)} />
-        </div>
-        <div className = "board-row">
-          <Square value={names[4]} buttonState={gameState[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={names[5]} buttonState={gameState[5]} onSquareClick={() => handleClick(5)} />
-          <Square value={names[6]} buttonState={gameState[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={names[7]} buttonState={gameState[7]} onSquareClick={() => handleClick(7)} />
-        </div>
-        <div className = "board-row">
-          <Square value={names[8]} buttonState={gameState[8]} onSquareClick={() => handleClick(8)} />
-          <Square value={names[9]} buttonState={gameState[9]} onSquareClick={() => handleClick(9)} />
-          <Square value={names[10]} buttonState={gameState[10]} onSquareClick={() => handleClick(10)} />
-          <Square value={names[11]} buttonState={gameState[11]} onSquareClick={() => handleClick(11)} />
-        </div>
-        <div className = "board-row center">
-          <Square value={names[12]} buttonState={gameState[12]} onSquareClick={() => handleClick(12)} />
-          <Square value={names[13]} buttonState={gameState[13]} onSquareClick={() => handleClick(13)} />
-          <Square value={names[14]} buttonState={gameState[14]} onSquareClick={() => handleClick(14)} />
-          <Square value={names[15]} buttonState={gameState[15]} onSquareClick={() => handleClick(15)} />
-        </div>
-        <div className = "bttn-row">
-          <SubmitButton activeButtons={gameState.filter(item => item.isActive)} onSubClick={() => checkMatch()}/>
-          <DeselectButton activeButtons={gameState.filter(item => item.isActive)} onResClick = {() => deselect()}/>
-        </div>
+    <div className="wrapper">
+    <div className="board-game">
+      <div className="board">
+      {gameState.map((square, index) => (
+          <Square
+            buttonState={square}
+            onSquareClick={() => handleClick(index)}
+          />
+        ))}
       </div>
-    </>
-  )
+      <div className="bttn-row">
+        <SubmitButton
+          activeButtons={gameState.filter((item) => item.isActive)}
+          onSubClick={() => checkMatch()}
+        />
+        <DeselectButton
+          activeButtons={gameState.filter((item) => item.isActive)}
+          onResClick={() => deselect()}
+        />
+        <ShuffleButton
+          activeButtons={gameState.filter((item) => item.isActive)}
+          onShufClick={() => shuffle()}
+        />        
+      </div>
+    </div>
+    </div>
+  );
 }
 
 export default function Game() {
-  const [selectedCount, setSelectedCount] = useState(0);
+
   return (
     <>
       <div>
-        <Board /> 
+        <Board />
       </div>
     </>
   )
